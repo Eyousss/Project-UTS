2b45fe6c502ac28047bf6816881c40a766ce17df
Eyousss <aurelio.tk27@gmail.com>
2026-06-08 18:14:04 +0700

    Revert "salah"
    This reverts commit ebffae8f95cf1fa9eb594b641e5ed4fc3b45d205.

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home — Noma Coffee & Taichan</title>
-    <link rel="stylesheet" href="./css/Homepage.css">
+    <link rel="stylesheet" href="/css/Homepage.css">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>

5358791525bf86e61956b7e9a2b16f1809a81aaa
Eyousss <aurelio.tk27@gmail.com>
2026-06-08 23:31:29 +0700

    mengubah footer index, menu, feedback, gallery

---
+++ footer.php
@@ -0,0 +1,9 @@
+    <footer>
+        <label>&copy; 2026 Noma Coffee & Taichan. All rights reserved.</label><br>
+        <div class="sosmed-grup">
+        <a class="sosmed" href="https://www.instagram.com/noma_idn/" target="_blank"> 
+            <i class="fa fa-instagram" style="font-size:24px"></i>@noma_idn</a>
+        <a class="sosmed" href="https://web.whatsapp.com/send/?phone=%2B6285138263206&text&type=phone_number&app_absent=0" target="_blank"> 
+            <i class="fa fa-whatsapp" style="font-size:24px"></i>+62 851-3826-3206</a>
+        </div>    
+        </footer>

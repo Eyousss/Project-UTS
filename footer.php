8a79c5996329ec7f15f137e2268ed063887cde4e
Eyousss <aurelio.tk27@gmail.com>
2026-06-22 22:46:18 +0700

    mengubah footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="/css/footer.css">
     <footer>
         <label>&copy; 2026 Noma Coffee & Taichan. All rights reserved.</label><br>
         <div class="sosmed-grup">
